a65d7882e052a00fc252f2948b8a25e55eacbcb3
bailuoxi66 <bailuoxihaha@163.com>
2022-06-01 20:25:01 +0800

    从parameter tool里面解析启动参数

--- src/main/java/com/example/wc/StreamWordCount.java
+++ src/main/java/com/example/wc/StreamWordCount.java
@@ -1,6 +1,7 @@
 package com.example.wc;
 
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -13,9 +14,15 @@ public class StreamWordCount {
     public static void main(String[] args) throws Exception {
         // 创建流处理执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
 
         // 从socket 中读取数据
-        DataStream<String> inputDataStream = env.socketTextStream("localhost", 7777);
+        // 使用parameter tool工具从程序启动参数中提取配置项
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+        String host = parameterTool.get("host");
+        int port = parameterTool.getInt("port");
+
+        DataStream<String> inputDataStream = env.socketTextStream(host, port);
 
         // keyBy是根据当前key的hashcode对数据进行重分区的操作
         SingleOutputStreamOperator<Tuple2<String, Integer>> resultStream =
