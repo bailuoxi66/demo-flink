bcd9065e566a455c4f5dd461a0065e93bd4003b7
bailuoxi66 <bailuoxihaha@163.com>
2022-08-06 02:40:23 +0800

    流处理API_Transform(一) 基本转换操作

---
+++ src/main/java/com/example/apitest/transform/TransformTest1_Base.java
@@ -0,0 +1,56 @@
+package com.example.apitest.transform;
+
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+/**
+ * @author laneliang
+ */
+public class TransformTest1_Base {
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        // 从文件读取数据
+        DataStreamSource<String> inputStream = env.readTextFile("E:\\Tech\\demo-flink\\src\\main\\resources\\sensor.txt");
+
+        // 1. map, 把String转换成长度输出
+        DataStream<Integer> mapStream = inputStream.map(new MapFunction<String, Integer>() {
+            @Override
+            public Integer map(String s) throws Exception {
+                return s.length();
+            }
+        });
+
+        // 2. flatmap, 按逗号分字段
+        DataStream<String> flatMapStream = inputStream.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public void flatMap(String s, Collector<String> collector) throws Exception {
+                String[] fields = s.split(",");
+                for (String field : fields) {
+                    collector.collect(field);
+                }
+            }
+        });
+
+        // 3. filter, 筛选sensor_1开头的id对应的数据
+        DataStream<String> filterStream = inputStream.filter(new FilterFunction<String>() {
+            @Override
+            public boolean filter(String s) throws Exception {
+                return s.startsWith("sensor_1");
+            }
+        });
+
+        mapStream.print("map");
+        flatMapStream.print("flatMap");
+        filterStream.print("filter");
+
+        env.execute();
+    }
+}
