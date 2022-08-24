7a3cdcecf489b266140067638bf4e3cb6f045b3e
bailuoxi66 <bailuoxihaha@163.com>
2022-08-24 22:54:10 +0800

    RichFuntion富函数

---
+++ src/main/java/com/example/apitest/transform/TransformTest5_RichFunction.java
@@ -0,0 +1,51 @@
+package com.example.apitest.transform;
+
+import com.example.apitest.beans.SensorReading;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class TransformTest5_RichFunction {
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(4);
+
+        // 从文件读取数据
+        DataStreamSource<String> inputStream = env.readTextFile("E:\\Tech\\TTTTT\\demo-flink\\src\\main\\resources\\sensor.txt");
+        SingleOutputStreamOperator<SensorReading> dataStream = inputStream.map(line -> {
+            String[] fields = line.split(",");
+            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
+        });
+
+        DataStream<Tuple2<String, Integer>> resultStreams = dataStream.map(new MyMapper());
+        resultStreams.print();
+
+        env.execute();
+
+    }
+
+    public static class MyMapper extends RichMapFunction<SensorReading, Tuple2<String, Integer>> {
+
+        @Override
+        public Tuple2<String, Integer> map(SensorReading value) throws Exception {
+            return new Tuple2<>(value.getId(), getRuntimeContext().getIndexOfThisSubtask());
+        }
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            // 初始化工作，一般是定义状态，或者建立数据库连接
+            System.out.println("open");
+        }
+
+        @Override
+        public void close() throws Exception {
+            // 一般是关闭连接和清空状态的收尾操作
+            System.out.println("close");
+        }
+    }
+}
