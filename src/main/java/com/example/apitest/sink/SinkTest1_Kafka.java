9047076e55af800330310820eeddd18372506d25
bailuoxi66 <bailuoxihaha@163.com>
2022-08-25 20:48:37 +0800

    sink kafka

---
+++ src/main/java/com/example/apitest/sink/SinkTest1_Kafka.java
@@ -0,0 +1,26 @@
+package com.example.apitest.sink;
+
+import com.example.apitest.beans.SensorReading;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
+
+public class SinkTest1_Kafka {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        // 从文件读取数据
+        DataStreamSource<String> inputStream = env.readTextFile("C:\\Users\\laneliang\\IdeaProjects\\flinktutorial\\src\\main\\resources\\sourceFile.txt");
+        SingleOutputStreamOperator<String> dataStream = inputStream.map(line -> {
+            String[] fields = line.split(",");
+            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2])).toString();
+        });
+
+        dataStream.addSink(new FlinkKafkaProducer011<String>("localhost:9092", "sinktest", new SimpleStringSchema()));
+
+        env.execute();
+    }
+}
