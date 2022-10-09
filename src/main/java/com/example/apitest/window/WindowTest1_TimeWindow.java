e895193f2f0652cb52c0b81cbd73dc8293a1c131
bailuoxi66 <bailuoxihaha@163.com>
2022-10-10 01:38:01 +0800

    时间窗口-增量函数

---
+++ src/main/java/com/example/apitest/window/WindowTest1_TimeWindow.java
@@ -0,0 +1,54 @@
+package com.example.apitest.window;
+
+import com.example.apitest.beans.SensorReading;
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+public class WindowTest1_TimeWindow {
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(4);
+
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+        String host = parameterTool.get("host");
+        int port = parameterTool.getInt("port");
+
+        DataStream<String> inputStream = env.socketTextStream(host, port);
+
+        DataStream<SensorReading> dataStream = inputStream.map(line -> {
+            String[] fields = line.split(",");
+            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
+        });
+
+        DataStream<Integer> res = dataStream.keyBy("id").timeWindow(Time.seconds(15))
+                .aggregate(new AggregateFunction<SensorReading, Integer, Integer>() {
+
+                    @Override
+                    public Integer createAccumulator() {
+                        return 0;
+                    }
+
+                    @Override
+                    public Integer add(SensorReading value, Integer accumulator) {
+                        return accumulator + 1;
+                    }
+
+                    @Override
+                    public Integer getResult(Integer accumulator) {
+                        return accumulator;
+                    }
+
+                    @Override
+                    public Integer merge(Integer a, Integer b) {
+                        return a + b;
+                    }
+                });
+
+        res.print();
+        env.execute();
+    }
+}
