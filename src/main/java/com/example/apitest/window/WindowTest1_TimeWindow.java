728b518a60a45eb67d8e4b696548ba62f624ea29
bailuoxi66 <bailuoxihaha@163.com>
2022-10-10 01:57:14 +0800

    时间窗口-全窗口函数

--- src/main/java/com/example/apitest/window/WindowTest1_TimeWindow.java
+++ src/main/java/com/example/apitest/window/WindowTest1_TimeWindow.java
@@ -1,11 +1,19 @@
 package com.example.apitest.window;
 
 import com.example.apitest.beans.SensorReading;
+import org.apache.commons.collections.IteratorUtils;
 import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.IterableUtils;
 
 public class WindowTest1_TimeWindow {
     public static void main(String[] args) throws Exception {
@@ -48,7 +56,20 @@ public class WindowTest1_TimeWindow {
                     }
                 });
 
+        // 全窗口函数
+        DataStream<Tuple3<String, Long, Integer>> res2 = dataStream.keyBy("id").timeWindow(Time.seconds(15))
+                .apply(new WindowFunction<SensorReading, Tuple3<String, Long, Integer>, Tuple, TimeWindow>() {
+                    @Override
+                    public void apply(Tuple tuple, TimeWindow window, Iterable<SensorReading> input, Collector<Tuple3<String, Long, Integer>> out) throws Exception {
+                        String id = tuple.getField(0);
+                        Long windowId = window.getEnd();
+                        Integer count = IteratorUtils.toList(input.iterator()).size();
+                        out.collect(new Tuple3<>(id, windowId, count));
+                    }
+                });
+
         res.print();
+        res2.print();
         env.execute();
     }
 }
