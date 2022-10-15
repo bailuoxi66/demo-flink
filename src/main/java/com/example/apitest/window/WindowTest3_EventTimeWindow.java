1135da211568fa576956b8554f1899a590ac687f
bailuoxi66 <bailuoxihaha@163.com>
2022-10-15 11:56:30 +0800

    事件事件语义下的窗口测试-允许迟到数据和侧输出流验证

--- src/main/java/com/example/apitest/window/WindowTest3_EventTimeWindow.java
+++ src/main/java/com/example/apitest/window/WindowTest3_EventTimeWindow.java
@@ -15,6 +15,7 @@ import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
 
 public class WindowTest3_EventTimeWindow {
     public static void main(String[] args) throws Exception {
@@ -48,9 +49,17 @@ public class WindowTest3_EventTimeWindow {
                 })
                 ;
 
-        SingleOutputStreamOperator<SensorReading> minTemperature = dataStream.keyBy("id").timeWindow(Time.seconds(15))
+        OutputTag<SensorReading> outputTag = new OutputTag<SensorReading>("late") {
+        };
+
+        SingleOutputStreamOperator<SensorReading> minTemperature = dataStream.keyBy("id")
+                .timeWindow(Time.seconds(15))
+                .allowedLateness(Time.minutes(1))
+                .sideOutputLateData(outputTag)
                 .minBy("temperature");
         minTemperature.print("minTemp");
+        minTemperature.getSideOutput(outputTag).print("late");
+
         env.execute();
     }
 }
