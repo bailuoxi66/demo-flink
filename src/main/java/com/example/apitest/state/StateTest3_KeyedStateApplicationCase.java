bfbaf3a2105d2533a97da9c2b1953ff212bd878c
bailuoxi66 <bailuoxihaha@163.com>
2022-10-22 15:01:30 +0800

    实现温度跳变报警

---
+++ src/main/java/com/example/apitest/state/StateTest3_KeyedStateApplicationCase.java
@@ -0,0 +1,73 @@
+package com.example.apitest.state;
+
+import com.example.apitest.beans.SensorReading;
+import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+public class StateTest3_KeyedStateApplicationCase {
+    public static void main(String[] args) throws Exception {
+
+        // 创建流处理执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        // 从socket读取数据
+        DataStream<String> inputDataStream = env.socketTextStream("localhost", 7772);
+
+
+        DataStream<SensorReading> dataStream = inputDataStream.map(line -> {
+            String[] fields = line.split(",");
+            return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
+        });
+
+        SingleOutputStreamOperator<Tuple3<String, Double, Double>> resultStream = dataStream.keyBy("id")
+                .flatMap(new TempChangeWarning(10.0));
+
+        resultStream.print();
+        env.execute();
+    }
+
+    public static class TempChangeWarning extends RichFlatMapFunction<SensorReading, Tuple3<String, Double, Double>> {
+
+        // 私有属性，温度跳变阈值
+        private Double threshold;
+
+        public TempChangeWarning(Double threshold){
+            this.threshold = threshold;
+        }
+
+        // 定义状态、保存上一次的温度值
+        private ValueState<Double> lastTempState;
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            lastTempState = getRuntimeContext().getState(new ValueStateDescriptor<Double>("last-temp", Double.class));
+        }
+
+        @Override
+        public void flatMap(SensorReading value, Collector<Tuple3<String, Double, Double>> out) throws Exception {
+            // 获取上次的温度值
+            Double lastTemp = lastTempState.value();
+
+            if (lastTemp != null) {
+                Double diff = Math.abs( lastTemp - value.getTemperature() );
+                if (diff > threshold) {
+                    out.collect(new Tuple3<>(value.getId(), value.getTemperature(), lastTemp));
+                }
+            }
+
+            // 更新传感器的结果
+            lastTempState.update(value.getTemperature());
+        }
+    }
+}
