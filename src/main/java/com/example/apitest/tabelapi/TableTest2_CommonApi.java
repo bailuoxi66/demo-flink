f885c41f53f60d17b5a4a0b291cb77ea12dcd5e3
bailuoxi66 <bailuoxihaha@163.com>
2022-10-29 18:13:53 +0800

    Table Sql创建表-从文件读取数据

--- src/main/java/com/example/apitest/tabelapi/TableTest2_CommonApi.java
+++ src/main/java/com/example/apitest/tabelapi/TableTest2_CommonApi.java
@@ -2,13 +2,19 @@ package com.example.apitest.tabelapi;
 
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.DataTypes;
 import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.TableEnvironment;
 import org.apache.flink.table.api.java.BatchTableEnvironment;
 import org.apache.flink.table.api.java.StreamTableEnvironment;
+import org.apache.flink.table.descriptors.Csv;
+import org.apache.flink.table.descriptors.FileSystem;
+import org.apache.flink.table.descriptors.Schema;
+import org.apache.flink.types.Row;
 
 public class TableTest2_CommonApi {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // 创建执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         // 设置并行度为1
@@ -40,5 +46,24 @@ public class TableTest2_CommonApi {
                 .inBatchMode()
                 .build();
         TableEnvironment blinkBatchTableEnv = TableEnvironment.create(blinkBatchSettings);
+
+        // 2. 表的创建：连接外部系统，读取数据
+        // 2.1 读取文件
+        String filePath = "E:\\Tech\\TTTTT\\demo-flink\\src\\main\\resources\\sensor.txt";
+        tableEnv.connect( new FileSystem().path(filePath))
+                .withFormat( new Csv())
+                .withSchema( new Schema()
+                        .field("id", DataTypes.STRING())
+                        .field("timestamp", DataTypes.BIGINT())
+                        .field("temp", DataTypes.DOUBLE()))
+                .createTemporaryTable("inputTable");
+
+        Table inputTable = tableEnv.from("inputTable");
+
+        inputTable.printSchema();
+        tableEnv.toAppendStream(inputTable, Row.class).print();
+
+        env.execute();
+
     }
 }
