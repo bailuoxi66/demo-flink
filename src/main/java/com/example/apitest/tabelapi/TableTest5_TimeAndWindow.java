872bb1aed90131c4c7c561a665cb3c28e948621c
bailuoxi66 <bailuoxihaha@163.com>
2022-11-04 01:28:18 +0800

    Table API 事件事件特性

--- src/main/java/com/example/apitest/tabelapi/TableTest5_TimeAndWindow.java
+++ src/main/java/com/example/apitest/tabelapi/TableTest5_TimeAndWindow.java
@@ -1,8 +1,11 @@
 package com.example.apitest.tabelapi;
 
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.java.StreamTableEnvironment;
 import org.apache.flink.types.Row;
@@ -15,6 +18,7 @@ public class TableTest5_TimeAndWindow {
         // 1. 创建环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
         StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
 
@@ -25,10 +29,16 @@ public class TableTest5_TimeAndWindow {
         DataStream<SensorReading> dataStream = inputStream.map(line -> {
             String[] str = line.split(",");
             return new SensorReading(str[0], new Long(str[1]), new Double(str[2]));
+        }).assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<SensorReading>(Time.seconds(2)) {
+            @Override
+            public long extractTimestamp(SensorReading element) {
+                return element.getTimestamp() * 1000L;
+            }
         });
 
         // 4. 将流转换成表，定义时间特性
-        Table dataTable = tableEnv.fromDataStream(dataStream, "id, timestamp as ts, temperature as temp, pt.proctime");
+        // Table dataTable = tableEnv.fromDataStream(dataStream, "id, timestamp as ts, temperature as temp, pt.proctime");
+        Table dataTable = tableEnv.fromDataStream(dataStream, "id, timestamp as ts, temperature as temp, rt.rowtime");
         dataTable.printSchema();
         tableEnv.toAppendStream(dataTable, Row.class).print();
 
