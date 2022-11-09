6d8714b34d8f544d1220814cc37e94195ce0d81b
bailuoxi66 <bailuoxihaha@163.com>
2022-11-10 01:11:19 +0800

    UV统计：程序框架和自定义窗口触发器

---
+++ UserBehaviorAnalysis/NetworkFlowAnalysis/src/main/java/com/example/networkflow_analysis/UvWithBloomFilter.java
@@ -0,0 +1,73 @@
+package com.example.networkflow_analysis;
+
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.triggers.Trigger;
+import org.apache.flink.streaming.api.windowing.triggers.TriggerResult;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+
+import java.net.URL;
+
+import com.example.networkflow_analysis.beans.PageViewCount;
+import com.example.networkflow_analysis.beans.UserBehavior;
+
+public class UvWithBloomFilter {
+    public static void main(String[] args) throws Exception {
+        // 1. 创建执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        // 设置并行度为1
+        env.setParallelism(1);
+
+        // 2. 从csv文件中获取数据
+        URL resource = PageView.class.getResource("/UserBehavior.csv");
+        DataStream<String> inputStream = env.readTextFile(resource.getPath());
+
+        // 3. 转换成POJO,分配时间戳和watermark
+        DataStream<UserBehavior> dataStream = inputStream.map(line -> {
+            String[] fields = line.split(",");
+            return new UserBehavior(new Long(fields[0]), new Long(fields[1]), new Integer(fields[2]), fields[3], new Long(fields[4]));
+        }).assignTimestampsAndWatermarks(new AscendingTimestampExtractor<UserBehavior>() {
+            @Override
+            public long extractAscendingTimestamp(UserBehavior element) {
+                return element.getTimestamp() * 1000L;
+            }
+        });
+
+        SingleOutputStreamOperator<PageViewCount> uvStream = dataStream.filter(data -> "pv".equals(data.getBehavior()))
+            .timeWindowAll(Time.hours(1))
+            .trigger(new MyTrigger())
+            .apply(new UvCountResultWithBloomFilter());
+
+        uvStream.print();
+
+        env.execute("uv count with bloom filter job");
+    }
+
+    public static class MyTrigger extends Trigger<UserBehavior, TimeWindow>{
+
+        @Override
+        public TriggerResult onElement(UserBehavior element, long timestamp, TimeWindow window, TriggerContext ctx) throws Exception {
+            return TriggerResult.FIRE_AND_PURGE;
+        }
+
+        @Override
+        public TriggerResult onProcessingTime(long time, TimeWindow window, TriggerContext ctx) throws Exception {
+            return TriggerResult.CONTINUE;
+        }
+
+        @Override
+        public TriggerResult onEventTime(long time, TimeWindow window, TriggerContext ctx) throws Exception {
+            return TriggerResult.CONTINUE;
+        }
+
+        @Override
+        public void clear(TimeWindow window, TriggerContext ctx) throws Exception {
+
+        }
+    }
+}
