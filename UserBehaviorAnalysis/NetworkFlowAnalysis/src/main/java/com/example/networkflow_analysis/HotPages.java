d3ed6ea93d1e969aa1d39d1bec70baff54e748d2
bailuoxi66 <bailuoxihaha@163.com>
2022-11-09 13:24:49 +0800

    实时热门页面统计：代码具体实现

--- UserBehaviorAnalysis/NetworkFlowAnalysis/src/main/java/com/example/networkflow_analysis/HotPages.java
+++ UserBehaviorAnalysis/NetworkFlowAnalysis/src/main/java/com/example/networkflow_analysis/HotPages.java
@@ -1,14 +1,27 @@
 package com.example.networkflow_analysis;
 
+import org.apache.commons.compress.utils.Lists;
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.api.windowing.windows.Window;
+import org.apache.flink.util.Collector;
 
 import java.net.URL;
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 import com.example.networkflow_analysis.beans.ApacheLogEvent;
 import com.example.networkflow_analysis.beans.PageViewCount;
@@ -41,6 +54,10 @@ public class HotPages {
         SingleOutputStreamOperator<PageViewCount> windowAggStream = dataStream
             // 过滤get请求
             .filter(data -> "GET".equals(data.getMethod()))
+            .filter(data -> {
+                String regex = "^((?!\\.(css|js|png|ico)$).)*$";
+                return Pattern.matches(regex, data.getUrl());
+            })
             // 按照url分组
             .keyBy(ApacheLogEvent::getUrl)
             .timeWindow(Time.minutes(10), Time.seconds(5))
@@ -53,5 +70,87 @@ public class HotPages {
         resultStream.print();
         env.execute("hot pages job");
 
+    }
+
+    public static class PageCountAgg implements AggregateFunction<ApacheLogEvent, Long, Long>{
+
+        @Override
+        public Long createAccumulator() {
+            return 0L;
+        }
+
+        @Override
+        public Long add(ApacheLogEvent value, Long accumulator) {
+            return accumulator + 1;
+        }
+
+        @Override
+        public Long getResult(Long accumulator) {
+            return accumulator;
+        }
+
+        @Override
+        public Long merge(Long a, Long b) {
+            return a + b;
+        }
+    }
+
+    // 实现自定义的窗口函数
+    public static class PageCountResult implements WindowFunction<Long, PageViewCount, String, TimeWindow>{
+
+        @Override
+        public void apply(String url, TimeWindow window, Iterable<Long> input, Collector<PageViewCount> out) throws Exception {
+            out.collect(new PageViewCount(url, window.getEnd(), input.iterator().next()));
+        }
+    }
+
+    // 实现自定义的处理函数
+    public static class TopNHotPages extends KeyedProcessFunction<Long, PageViewCount, String> {
+        private Integer topSize;
+
+        public TopNHotPages(Integer topSize) {
+            this.topSize = topSize;
+        }
+
+        // 定义状态，保存当前所有PageViewCount到list中
+        ListState<PageViewCount> pageViewCountListState;
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            pageViewCountListState = getRuntimeContext().getListState(new ListStateDescriptor<PageViewCount>("page-count-list", PageViewCount.class));
+        }
+
+        @Override
+        public void processElement(PageViewCount value, Context ctx, Collector<String> out) throws Exception {
+            pageViewCountListState.add(value);
+            ctx.timerService().registerEventTimeTimer(value.getWindowEnd() + 1);
+        }
+
+        @Override
+        public void onTimer(long timestamp, OnTimerContext ctx, Collector<String> out) throws Exception {
+            ArrayList<PageViewCount> pageViewCounts = Lists.newArrayList(pageViewCountListState.get().iterator());
+
+            pageViewCounts.sort((a, b) -> -Long.compare(a.getCount(), b.getCount()));
+
+            // 格式化成String输出
+            StringBuilder resultBuilder = new StringBuilder();
+            resultBuilder.append("============================").append(System.lineSeparator());
+            resultBuilder.append("窗口结束时间：").append(new Timestamp(timestamp - 1)).append(System.lineSeparator());
+
+            // 遍历列表，取top n输出
+            for (int i = 0; i < Math.min(topSize, pageViewCounts.size()); i++) {
+                PageViewCount pageViewCount = pageViewCounts.get(i);
+                resultBuilder.append("NO ").append(i + 1).append(":")
+                    .append(" 页面URL = ").append(pageViewCount.getUrl())
+                    .append(" 浏览量 = ").append(pageViewCount.getCount())
+                    .append(System.lineSeparator());
+            }
+            resultBuilder.append("===============================").append(System.lineSeparator());
+
+            // 控制输出频率
+            Thread.sleep(1000L);
+
+            out.collect(resultBuilder.toString());
+        }
     }
 }
