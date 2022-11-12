2fb7845c59a96685d455373c5c47cc3528013eb1
bailuoxi66 <bailuoxihaha@163.com>
2022-11-13 00:14:33 +0800

    恶意登录检测：简单代码实现

--- UserBehaviorAnalysis/LoginFailDetect/src/main/java/com/example/loginfail_detect/LoginFail.java
+++ UserBehaviorAnalysis/LoginFailDetect/src/main/java/com/example/loginfail_detect/LoginFail.java
@@ -1,39 +1,121 @@
 package com.example.loginfail_detect;
 
+import org.apache.commons.compress.utils.Lists;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.TimeCharacteristic;
-import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
 
 import java.net.URL;
+import java.util.ArrayList;
 
 import com.example.loginfail_detect.beans.LoginEvent;
+import com.example.loginfail_detect.beans.LoginFailWarning;
 
+/**
+ * @author : Ashiamd email: ashiamd@foxmail.com
+ * @date : 2021/2/6 1:49 AM
+ */
 public class LoginFail {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
         env.setParallelism(1);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
+        // 1. 从文件中读取数据
         URL resource = LoginFail.class.getResource("/LoginLog.csv");
-        DataStream<LoginEvent> loginEventStream = env.readTextFile(resource.getPath())
+        SingleOutputStreamOperator<LoginEvent> loginEventStream = env.readTextFile(resource.getPath())
             .map(line -> {
                 String[] fields = line.split(",");
                 return new LoginEvent(new Long(fields[0]), fields[1], fields[2], new Long(fields[3]));
             }).assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<LoginEvent>(Time.seconds(3)) {
                 @Override
                 public long extractTimestamp(LoginEvent element) {
-                    return element.getTimestamp() * 1000L;
+                    return element.getTimestamp();
                 }
             });
-
         // 自定义处理函数检测连续登录失败事件
-        SingleOutputStreamOperator<Object> warningStream = loginEventStream.keyBy(LoginEvent::getUserId)
-            .process(new LoginFailDetectWarning());
+        SingleOutputStreamOperator<LoginFailWarning> warningStream = loginEventStream
+            .keyBy(LoginEvent::getUserId)
+            .process(new LoginFailDetectWarning(2));
 
         warningStream.print();
+
         env.execute("login fail detect job");
+    }
+
+    // 实现自定义KeyedProcessFunction
+    public static class LoginFailDetectWarning extends KeyedProcessFunction<Long, LoginEvent, LoginFailWarning> {
+        // 定义属性，最大连续登录失败次数
+        private Integer maxFailTimes;
+
+        // 定义状态：保存2秒内所有的登录失败事件
+        ListState<LoginEvent> loginFailEventListState;
+        // 定义状态：保存注册的定时器时间戳
+        ValueState<Long> timerTsState;
+
+        public LoginFailDetectWarning(Integer maxFailTimes) {
+            this.maxFailTimes = maxFailTimes;
+        }
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            loginFailEventListState = getRuntimeContext().getListState(new ListStateDescriptor<LoginEvent>("login-fail-list", LoginEvent.class));
+            timerTsState = getRuntimeContext().getState(new ValueStateDescriptor<Long>("timer-ts", Long.class));
+        }
+
+        @Override
+        public void onTimer(long timestamp, OnTimerContext ctx, Collector<LoginFailWarning> out) throws Exception {
+            // 定时器触发，说明2秒内没有登录成功，判读ListState中失败的个数
+            ArrayList<LoginEvent> loginFailEvents = Lists.newArrayList(loginFailEventListState.get().iterator());
+            int failTimes = loginFailEvents.size();
+
+            if (ctx.getCurrentKey() == 1035){
+                System.out.println("============" + failTimes);
+            }
+
+            if (failTimes >= maxFailTimes) {
+                // 如果超出设定的最大失败次数，输出报警
+                out.collect(new LoginFailWarning(ctx.getCurrentKey(),
+                    loginFailEvents.get(0).getTimestamp(),
+                    loginFailEvents.get(failTimes - 1).getTimestamp(),
+                    "login fail in 2s for " + failTimes + " times"));
+            }
+
+            // 清空状态
+            loginFailEventListState.clear();
+            timerTsState.clear();
+        }
+
+        @Override
+        public void processElement(LoginEvent value, Context ctx, Collector<LoginFailWarning> out) throws Exception {
+            // 判断当前登录事件类型
+            if ("fail".equals(value.getLoginState())) {
+                // 1. 如果是失败事件，添加到表状态中
+                loginFailEventListState.add(value);
+
+                // 如果没有定时器，注册一个2秒之后的定时器
+                if (null == timerTsState.value()) {
+                    long ts = (value.getTimestamp() + 2) * 1000L;
+                    ctx.timerService().registerEventTimeTimer(ts);
+                    timerTsState.update(ts);
+                }
+            } else {
+                // 2. 如果是登录成功，删除定时器，清空状态，重新开始
+                if (null != timerTsState.value()) {
+                    ctx.timerService().deleteEventTimeTimer(timerTsState.value());
+                }
+                loginFailEventListState.clear();
+                timerTsState.clear();
+            }
+        }
     }
 }
